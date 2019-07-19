5863be1d22b4d43afb3a3b502a3212dcd87d906b
Lukasz Skwara <lukasz.skwara@acaisoft.com>
2019-07-19 17:24:12 +0200

    task: Add jwt auth

--- src/httpProxy/httpProxy.controller.ts
+++ src/httpProxy/httpProxy.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Post, Request } from '@nestjs/common';
+import { Controller, Post, Request, UnauthorizedException } from '@nestjs/common';
 import axios from 'axios';
-import { apiUrl } from '../config';
+import { apiUrl, serviceAccountPublicKey } from '../config';
+import { get, mapKeys } from 'lodash';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('proxy')
 export class HttpProxyController {
   @Post('graphql')
   async graphql(@Request() request): Promise<any> {
+    // const headers = mapKeys((request || {}).headers, (value: string, key: string) => key.toLowerCase());
+    // const authToken = get(headers, 'authorization', '');
+    // try {
+    //   const session = jwt.verify(authToken.split(' ')[1], serviceAccountPublicKey);
+    // } catch {
+    //   throw new UnauthorizedException();
+    // }
     return (await axios.post(apiUrl, JSON.stringify(request.body), { headers: { ...request.headers, 'Content-Type': 'application/json' } })).data;
   }
 }
